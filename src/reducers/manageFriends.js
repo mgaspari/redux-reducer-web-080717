d145c47fee1f7d29679b9577c3076af553a0e34d
mgaspari <marcoanthonygaspari@gmail.com>
2017-10-26 10:11:50 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+  switch(action.type){
+    case "ADD_FRIEND":
+      return {friends: [...state.friends, action.friend ]}
+      break
+    case "REMOVE_FRIEND":
+      return {friends: state.friends.filter((fri)=> {
+        return fri.id !== action.id
+      })}
+      break
+    default:
+    return state
+  }
 }
